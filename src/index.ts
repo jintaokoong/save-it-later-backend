57a2e691e114a57932d8a6cb080b3293f34a63e8
Jintao Koong <jintaokoong99@gmail.com>
2021-10-17 14:04:38 +0800

    feat: add readability dependencies for node

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import { Readability } from '@mozilla/readability';
+import * as JSDOM from 'jsdom'
+
 const main = () => {
-  console.log('hello world');
+  const doc = new JSDOM.JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
+    url: "https://www.example.com/the-page-i-got-the-source-from"
+  });
+  const reader = new Readability(doc.window.document);
+  const article = reader.parse();
+  console.log(article);
 }
 
 main()
