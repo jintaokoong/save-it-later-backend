66534bd24d78cf3e124d19ed8458d0bf56dc073f
Jintao Koong <jintaokoong99@gmail.com>
2021-10-17 14:16:17 +0800

    feat: add fetch implement test run

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { Readability } from '@mozilla/readability';
 import * as JSDOM from 'jsdom'
+import fetch from 'cross-fetch';
+import * as fs from 'fs'
 
 const main = () => {
-  const doc = new JSDOM.JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
-    url: "https://www.example.com/the-page-i-got-the-source-from"
-  });
-  const reader = new Readability(doc.window.document);
-  const article = reader.parse();
-  console.log(article);
+  const url ='https://www.cincainews.com/news/tech-gadgets/2021/10/14/tm-stops-offering-public-ip-for-customers-on-unifi-100mbps-plan-and-below/2013227';
+  fetch(url)
+    .then(res => res.text())
+    .then(t => {
+    const doc = new JSDOM.JSDOM(t, {
+      url: url
+    });
+    const reader = new Readability(doc.window.document);
+    const article = reader.parse();
+    if (article) {
+      fs.writeFile('content.html', article.content, (err) => {
+        console.error(err);
+      });
+    }
+  })
+
 }
 
 main()
