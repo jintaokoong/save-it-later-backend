85280b9d4353581a29b63744cd33de9389d5e2c3
Jintao Koong <jintaokoong99@gmail.com>
2021-10-17 15:46:53 +0800

    feat: add dependencies
    - add dependencies
      - koa
      - koa router
      - koa logger
      - koa json
    - add hello world

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,27 @@ import { Readability } from '@mozilla/readability';
 import * as JSDOM from 'jsdom'
 import DOMPurify from 'isomorphic-dompurify';
 import fetch from 'cross-fetch';
-import * as fs from 'fs'
+import * as fs from 'fs';
+import koa from 'koa';
+import logger from 'koa-logger';
+import json from 'koa-json';
+import router from '@koa/router';
+
+const app = new koa();
+const r = new router();
+
+r.get('/', async (ctx, next) => {
+  ctx.body = { msg: 'hello world!' };
+  await next();
+})
+
+app.use(json());
+app.use(logger());
+app.use(r.routes()).use(r.allowedMethods());
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
 
 const main = () => {
   const url ='https://levelup.gitconnected.com/how-to-properly-set-up-express-with-typescript-1b52570677c9';
@@ -22,9 +42,3 @@ const main = () => {
     }
   })
 }
-
-const run = () => {
-  return;
-}
-
-run()
