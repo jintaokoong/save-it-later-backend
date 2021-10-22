df1416fb1a3a54bd6725881a53ac6485f8d38f8d
Jintao Koong <jintaokoong99@gmail.com>
2021-10-22 22:49:33 +0800

    chore: change project name

--- src/models/article.ts
+++ src/models/article.ts
@@ -1,20 +1,22 @@
-import S, { Sequelize, DataTypes, Model } from 'sequelize'
+import S, { Sequelize, DataTypes, Model } from "sequelize";
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: '/data/readability/database.sqlite',
+  dialect: "sqlite",
+  storage: "/data/sil-backend/database.sqlite",
 });
 
-export class Article extends Model {
-}
+export class Article extends Model {}
 
-Article.init({
-  id: { type: DataTypes.UUIDV4, primaryKey: true, defaultValue: S.UUIDV4, },
-  url: { type: DataTypes.STRING },
-  title: { type: DataTypes.STRING },
-  content: { type: DataTypes.STRING },
-  textContent: { type: DataTypes.STRING },
-  createdBy: { type: DataTypes.STRING, allowNull: false }
-}, {
-  sequelize: sequelize,
-  timestamps: true,
-})
+Article.init(
+  {
+    id: { type: DataTypes.UUIDV4, primaryKey: true, defaultValue: S.UUIDV4 },
+    url: { type: DataTypes.STRING },
+    title: { type: DataTypes.STRING },
+    content: { type: DataTypes.STRING },
+    textContent: { type: DataTypes.STRING },
+    createdBy: { type: DataTypes.STRING, allowNull: false },
+  },
+  {
+    sequelize: sequelize,
+    timestamps: true,
+  },
+);
